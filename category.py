663518f74c250fea9cf77bd9a2f3bacd8e20e073
EAmbrozas <ernestasambrozas@gmail.com>
2023-01-15 15:33:25 +0000

    Create category.py file and add word categories for Hangman game

---
+++ category.py
@@ -0,0 +1,60 @@
+# Hangman game word category list
+RETRO_GAMES = [
+    "super-mario",
+    "tetris",
+    "pac-man",
+    "sonic-the-hedgehog",
+    "donkey-kong",
+    "duck-hunt",
+    "space-invaders",
+    "pong",
+    "asteroids",
+    "duke-nukem",
+    "doom",
+    "galaxian"
+]
+
+CAR_BRANDS = [
+    "lamborghini",
+    "ford-mustang",
+    "mercedes-benz",
+    "aston-martin",
+    "cadillac",
+    "maserati",
+    "rolls-royce",
+    "alfa-romeo",
+    "bentley",
+    "cadillac",
+    "chevrolet",
+    "chrysler"
+]
+
+FOODS = [
+    "hamburger",
+    "hot-dog",
+    "french-fries",
+    "ice-cream",
+    "pizza",
+    "tortilla",
+    "tacos",
+    "ramen",
+    "sushi",
+    "lasagna",
+    "apple-pie",
+    "spring-roll"
+]
+
+ANIMALS = [
+    "squirrel",
+    "hippo",
+    "gorilla",
+    "koala",
+    "cheetah",
+    "leopard",
+    "giraffe",
+    "hedgehog",
+    "camel",
+    "hamster",
+    "raccoon",
+    "rhinoceros"
+]
