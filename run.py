e6e8e0057e8fcf3bab5c846a179f3da27470b7ca
EAmbrozas <ernestasambrozas@gmail.com>
2023-01-15 15:55:08 +0000

    Import word categories, difficulty levels and welcome function to run.py file

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Welcome message from welcome.py
+from welcome import welcome
+
+# Category list from category.py
+from category import RETRO_GAMES, CAR_BRANDS, FOODS, ANIMALS
+
+# Difficulty levels from difficulty.py
+from difficulty import EASY, MEDIUM, HARD
+
+
+welcome()
