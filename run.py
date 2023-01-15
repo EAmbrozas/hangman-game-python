9a3789e2600a0597339149eecac926b5147ed40e
EAmbrozas <ernestasambrozas@gmail.com>
2023-01-15 16:44:45 +0000

    Add choose_category function to run.py file

--- run.py
+++ run.py
@@ -8,11 +8,53 @@ from category import RETRO_GAMES, CAR_BRANDS, FOODS, ANIMALS
 from difficulty import EASY, MEDIUM, HARD
 
 
+def choose_category():
+    """
+    Let's the user to choose word cetegory
+    to play with and return their choice.
+    """
+    print("""
+Choose category to play with from the list bellow:
+[1] Retro Games
+[2] Car Brands
+[3] Foods
+[4] Animals
+""")
+
+    while True:
+        chosen_category = input("Category: ")
+        if chosen_category in {"1", "2", "3", "4"}:
+            if chosen_category == "1":
+                category = RETRO_GAMES
+                print(category)
+                return category
+
+            if chosen_category == "2":
+                category = CAR_BRANDS
+                print(category)
+                return category
+
+            if chosen_category == "3":
+                category = FOODS
+                print(category)
+                return category
+
+            if chosen_category == "4":
+                category = ANIMALS
+                print(category)
+                return category
+
+        else:
+            choose_category()
+
+
 def main():
     """
     Run game functions
     """
+    global category
     welcome()
+    choose_category()
 
 
 main()
