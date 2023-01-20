f2fdf9b0c5fb4e69f142e979f445106db0291e58
EAmbrozas <ernestasambrozas@gmail.com>
2023-01-20 21:32:39 +0000

    Remove '-'sign between the words in category.py file

--- category.py
+++ category.py
@@ -1,28 +1,28 @@
 # Hangman game word category list
 RETRO_GAMES = [
-    "super-mario",
+    "super mario",
     "tetris",
-    "pac-man",
-    "sonic-the-hedgehog",
-    "donkey-kong",
-    "duck-hunt",
-    "space-invaders",
+    "pac man",
+    "sonic the hedgehog",
+    "donkey kong",
+    "duck hunt",
+    "space invaders",
     "pong",
     "asteroids",
-    "duke-nukem",
+    "duke nukem",
     "doom",
     "galaxian"
 ]
 
 CAR_BRANDS = [
     "lamborghini",
-    "ford-mustang",
-    "mercedes-benz",
-    "aston-martin",
+    "ford mustang",
+    "mercedes benz",
+    "aston martin",
     "cadillac",
     "maserati",
-    "rolls-royce",
-    "alfa-romeo",
+    "rolls royce",
+    "alfa romeo",
     "bentley",
     "cadillac",
     "chevrolet",
@@ -31,17 +31,17 @@ CAR_BRANDS = [
 
 FOODS = [
     "hamburger",
-    "hot-dog",
-    "french-fries",
-    "ice-cream",
+    "hot dog",
+    "french fries",
+    "ice cream",
     "pizza",
     "tortilla",
     "tacos",
     "ramen",
     "sushi",
     "lasagna",
-    "apple-pie",
-    "spring-roll"
+    "apple pie",
+    "spring roll"
 ]
 
 ANIMALS = [
